d9123db2f621b1ed4b4e1b7136fd8d17bcd0d506
NeilSMyers <mmyersneil@gmail.com>
2021-01-20 11:14:30 -0700

    base business screens

---
+++ screens/BusinessProfile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const BusinessProfile = ({name, address, revenue}) => {
+  return (
+    <View>
+      <Text>business is serious business</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default BusinessProfile;
---
+++ screens/Businesses.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Businesses = () => {
+  return (
+    <View>
+      <Text>group</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Businesses;
