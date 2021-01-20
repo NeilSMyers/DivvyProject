04453b948f177291d4be63b94f29ef103a5ba6dd
NeilSMyers <mmyersneil@gmail.com>
2021-01-20 11:06:20 -0700

    3...2...1...GO

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Player 1 Start</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
